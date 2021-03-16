8e68b512fa9faedf2c7fe13638d0238edc0c000b
khan Tanveer <kt224945@gmail.com>
2021-03-16 17:01:19 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
+import { Button, FormControl, Input, InputLabel } from "@material-ui/core";
+import Message from "./Messages";
 
 function App() {
+  const [input, setInput] = useState("");
+  console.log(input);
+  const [messages, setMessages] = useState(["hello", "Khan", "Tanveer"]);
+  console.log(messages);
+  const [username, setUsername] = useState("");
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    //all the logic to send the message goes
+    setMessages([...messages, input]);
+    setInput("");
+  };
+
   return (
     <div className="App">
-      <h1>hello</h1>
-      <h3>I am khan Tanveer </h3>
-      fsd;fhsadjfhsaduojfbas;jdfbadjsfb;a
+      <h1>hello Khan Tanveer</h1>
+      <form>
+        <FormControl>
+          <InputLabel>Enter a message...</InputLabel>
+          <Input value={input} onChange={(e) => setInput(e.target.value)} />
+          <Button
+            disabled={!input}
+            variant="contained"
+            color="primary"
+            type="submit"
+            onClick={sendMessage}
+          >
+            Send Message{" "}
+          </Button>
+        </FormControl>
+      </form>
+
+      {/* messenges themselves */}
+
+      {messages.map((message) => (
+        <Message text={message} />
+      ))}
     </div>
   );
 }
