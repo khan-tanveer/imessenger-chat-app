7418d8cadcf9d62d342b2de64cb8b917a9718b05
khan Tanveer <kt224945@gmail.com>
2021-03-17 11:13:14 +0530

    commit

--- src/Messages.js
+++ src/Messages.js
@@ -2,15 +2,19 @@ import React from "react";
 import { Card, CardContent, Typography } from "@material-ui/core";
 import "./Message.css";
 
-const Messages = (props) => {
+const Messages = ({ message, username }) => {
+  const isUser = username === message.username;
+
   return (
-    <Card className="message">
-      <CardContent>
-        <Typography color="white" variant="h5" component="h2">
-          {props.username}: {props.text}
-        </Typography>
-      </CardContent>
-    </Card>
+    <div className={`message ${isUser && "message__user"}`}>
+      <Card className={isUser ? "message__userCard" : "message__guestCard"}>
+        <CardContent>
+          <Typography color="white" variant="h5" component="h2">
+            {message.username}: {message.text}
+          </Typography>
+        </CardContent>
+      </Card>
+    </div>
   );
 };
 
