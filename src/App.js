5a3efabc91ea6e238a62c8d9309e1cd7be08fc09
khan Tanveer <kt224945@gmail.com>
2021-03-19 21:32:43 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import { Button, FormControl, Input, InputLabel } from "@material-ui/core";
-import Message from "./Messages";
 import db from "./firebase";
+import Messages from "./Messages";
+import firebase from "firebase";
 
 function App() {
   const [input, setInput] = useState("");
-  console.log(input);
+  // console.log(input);
   const [messages, setMessages] = useState([
     { username: "soonny", message: "hey guys" },
     { username: "qazi", message: "whatsupp" },
   ]);
-  console.log(messages);
+  // console.log(messages);
   const [username, setUsername] = useState("");
 
   // console.log(setMessage());
 
   useEffect(() => {
     //run when apps components loads
-    db.collection("messages").onSnapshot((snapshot) => {
-      setMessages(snapshot.docs.map((doc) => doc.data()));
-    });
+    db.collection("messages")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) => {
+        // console.log(snapshot.docs.map((doc) => doc.data()));
+        setMessages(snapshot.docs.map((doc) => doc.data()));
+      });
   }, []);
 
   useEffect(() => {
@@ -30,8 +34,15 @@ function App() {
 
   const sendMessage = (e) => {
     e.preventDefault();
+
+    db.collection("messages").add({
+      message: input,
+      username: username,
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+
     //All the logic to send the message goes
-    setMessages([...messages, { username: username, text: input }]);
+    // setMessages([...messages, { username: username, text: input }]);
     setInput("");
   };
 
@@ -42,7 +53,7 @@ function App() {
 
       <form>
         <FormControl>
-          <InputLabel>Enter a message...</InputLabel>
+          <InputLabel>Enter a Message...</InputLabel>
           <Input value={input} onChange={(e) => setInput(e.target.value)} />
           <Button
             disabled={!input}
@@ -59,7 +70,7 @@ function App() {
       {/* messenges themselves */}
 
       {messages.map((message) => (
-        <Message username={username} message={message.message} />
+        <Messages username={username} message={message} />
       ))}
     </div>
   );
